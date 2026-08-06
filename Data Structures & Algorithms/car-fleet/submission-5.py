87bb1a2c28505a60aabd17dec1d56a64e3c84274
Kamal Sherawat <55201291+sherawatkamal@users.noreply.github.com>
2026-08-06 14:15:24 -0400

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        indices = sorted(range(len(position)), key=lambda i: position[i], reverse=True)
+
+        sorted_position = [position[i] for i in indices]
+        sorted_speed    = [speed[i] for i in indices]
+        
+        fleets = 0
+        prev_time = 0.0
+
+        for i in range(len(position)):
+            time = (target - sorted_position[i]) / sorted_speed[i]
+            if time > prev_time:
+                fleets += 1
+                prev_time = time
+
+
+        return fleets
+            
